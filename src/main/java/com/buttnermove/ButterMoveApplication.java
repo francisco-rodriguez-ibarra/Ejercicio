962cf8381f6e9e48383692348f114d159ea38753
francisco-rodriguez-ibarra <consafranco@gmail.com>
2023-06-12 00:16:40 -0700

    FirsCommit

---
+++ src/main/java/com/buttnermove/ButterMoveApplication.java
@@ -0,0 +1,13 @@
+package com.buttnermove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButterMoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButterMoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buttnermove/ButterMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buttnermove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButterMoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
